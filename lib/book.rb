df9dac489b0d426327192b56db277b5e10920fcc
CarsonRey <c.rey494@gmail.com>
2018-10-10 21:19:26 -0400

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,6 +1,8 @@
 class Book
-  attr_accessor :author, :page_count, :genre
-  attr_reader :title
+  attr_accessor :author, :page_count
+  attr_reader :title, :genre #we remove ':genre' from accessor to reader because if we don't need to edit it our reader we can automate it and save lines!
+
+  GENRES = []
 
   def initialize(title)
     @title = title
@@ -9,5 +11,10 @@ class Book
   def turn_page
     puts "Flipping the page...wow, you read fast!"
   end
+
+  def genre=(genre)
+    @genre = genre
+    GENRES << genre # we define a setter/writer method here because
+  end               # in addition to setting, we need to change GENRES
 
 end
