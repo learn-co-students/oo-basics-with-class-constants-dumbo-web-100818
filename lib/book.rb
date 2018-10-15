b13eff74bddcebd88e6335f2726ec9d9c512b14f
github username <github email address>
2018-10-15 13:42:25 -0400

    initialize method changed

--- lib/book.rb
+++ lib/book.rb
@@ -1,13 +1,27 @@
 class Book
-  attr_accessor :author, :page_count, :genre
+  attr_accessor :author, :page_count
   attr_reader :title
 
   def initialize(title)
     @title = title
   end
 
+  GENRES = []
+
+  # create the writer for genre and add the logic for the class constant
+  def genre=(genre)
+    @genre = genre
+    GENRES << genre
+  end
+
+
   def turn_page
     puts "Flipping the page...wow, you read fast!"
   end
 
 end
+
+
+# Explicitly define the genre= method, to integrate our class constant into the method
+# Remove the attr_accessor for :genre since we no longer need to generate a reader AND a writer.
+# Add an attr_reader for :genre, since we still want Ruby to generate a reader for us.
--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,9 +1,15 @@
+require 'pry'
 class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
 
-  def initialize(brand)
+  BRANDS = []
+  # binding.pry
+  def initialize(brand) #mandatory brand is created, so don't need writer method for brand
     @brand = brand
+    BRANDS.push(brand) if !BRANDS.include?(brand)
+
+    # BRANDS.uniq #this doesn't work. That's why we change it before it is pushed in.
   end
 
   def cobble
@@ -12,3 +18,9 @@ class Shoe
   end
 
 end
+
+
+# -git add . #add the changes you've made in the file tree
+# -git status #lets you see the files that are staged to be commited
+# -git commit -m ""   #this is adding a message on what you've udpated
+# -git push           #push to git
